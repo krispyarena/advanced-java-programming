5e81e518014b87b580c79a8437932b3b7475380e
Krishna Pandey <krispy.arena@gmail.com>
2023-12-31 13:46:11 +0545

    simple program for scaling a line  is added.

---
+++ src/graphicsPracticals/Scaling.java
@@ -0,0 +1,34 @@
+package graphicsPracticals;
+
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+class Scaling extends JFrame{
+  int x1 = 50, y1 = 50;
+  int x2 = 100, y2 = 100;
+
+  int sx = 4, int sy = 4;
+
+  int px1, py1, px2, py2;
+
+  public Scaling(){
+    setTitle("Scaling of Line");
+    setSize(500,500);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+    px1 = x1*sx;
+    py1 = y1*sy;
+
+    px2 = x2*sx;
+    py2 = y2*sy;
+  }
+
+  public void paint(Graphics graphics){
+    graphics.drawLine(x1,y1,x2,y2);
+    graphics.drawLine(px1, py1, px2, py2);
+  }
+
+  static public void main(String[] args){
+    new Scaling();
+  }
+}
