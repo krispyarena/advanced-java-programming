bc80d12e3f3670527609aed446c3f6924519f2ab
krispyarena <krispy.arena@gmail.com>
2024-01-31 19:59:27 +0545

    abstract class explained

--- src/oop/AbsClass.java
+++ src/oop/AbsClass.java
@@ -4,6 +4,8 @@ public abstract class AbsClass {
 
     // Abstrat class can contain both abstract and non abstract methods.
     // Interfaces can contain constants and abstract methods only.
+
+    // Abstract class can't be instantiated as it is incomplete thus we cant create an object of that class.
     
     abstract void display();
 
