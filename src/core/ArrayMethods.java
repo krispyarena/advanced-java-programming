2701034fab8102bd9c75ca8e0d148647072de939
krispyarena <krispy.arena@gmail.com>
2024-02-06 19:15:47 +0545

    different methods of java Arrays class is implemented

--- src/core/ArrayMethods.java
+++ src/core/ArrayMethods.java
@@ -8,24 +8,31 @@ public class ArrayMethods {
         
         int[] a = {4,3,8,1,0,5,10};
         int[] b = new int[7];
-        b[7] = 10;
+        b[6] = 10;
+        int [] c = new int [10]; 
 
         for (int i : a) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
 
-        //Arrays.fill(a, 1);
-        System.out.println("Index = " + Arrays.binarySearch(b, 100));
+        Arrays.fill(c, 1);
+        System.out.println("\nElements of Array C after fill method: ");
+        for (int i : c) {
+            System.out.print(i + " ");
+        }
+
         System.out.println(Arrays.equals(a,b));
         
         Arrays.sort(a);
 
-        System.arraycopy(a, 0, b, 0, 7);
+        System.arraycopy(a, 0, b, 0, 7  );
 
         System.out.println("After Sort: ");
 
         for (int i : b) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+
+        System.out.println("\nIndex = " + Arrays.binarySearch(b, 1));
     }
 }
